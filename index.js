b983e6eb070a3ac95d5b6964e3a52bfdf5a05f7b
JunhoMyung <74249745+JunhoMyung@users.noreply.github.com>
2021-07-15 02:17:38 +0900

    trial

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ const options = {
     cert: fs.readFileSync('cert.pem')
 };
 const https = require("https").createServer(options, app);
-// const io = require("socket.io")(https, {
-//   cors: {
-//     origin: "*",
-//     methods: "*",
-//   },
-// });
-
+const io = require("socket.io")(https, {
+  cors: {
+    origin: "*",
+    methods: "*",
+  },
+});
 
 http.listen(process.env.HTTP_PORT, () => {
   console.log(`listening on port ${process.env.HTTP_PORT}`);
 });
 https.listen(process.env.HTTPS_PORT, () => {
   console.log(`listening on port ${process.env.HTTPS_PORT}`);
+});
+
+io.on('connection', function (socket) {
+    console.log(' user connected');
+    socket.on('disconnect', function () {
+        console.log('a user disconnected');
+    });
 });
