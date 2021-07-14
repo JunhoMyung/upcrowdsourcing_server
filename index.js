d5a5913bc63f17c618454d9fbe9440c3ffbec692
JunhoMyung <74249745+JunhoMyung@users.noreply.github.com>
2021-07-15 00:27:02 +0900

    trial

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-require("dotenv").config();
-var app = require('express')();
-var http = require('http').Server(app)
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+  console.log('user disconnected');
+  });
 });
-
-http.listen(process.env.HTTP_PORT, () => {
-    console.log(`listening on port ${process.env.HTTP_PORT}`);
-});
-https.listen(process.env.HTTPS_PORT, () => {
-    console.log(`listening on port ${process.env.HTTPS_PORT}`);
+http.listen(80, () => {
+  console.log('Connected at 80');
 });
