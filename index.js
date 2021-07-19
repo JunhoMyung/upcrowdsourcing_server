d905a228652b363085f0d705a53f04fdb7d6ef7f
JunhoMyung <74249745+JunhoMyung@users.noreply.github.com>
2021-07-19 13:03:52 +0900

    error fix

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ let io = require("socket.io")(http, {
 io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("disconnected");
-  }
+  })
   socket.on("try", (greeting, greeting2) => {
     console.log("click")
   })
-}
+});
 
 http.listen(8080, () => {
   console.log(`listening on port ${process.env.PORT}`);
