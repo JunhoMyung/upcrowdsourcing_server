3e1e45b5fd0dc09fc746ae2324b43d370288322e
JunhoMyung <74249745+JunhoMyung@users.noreply.github.com>
2021-07-23 11:39:52 +0900

    error fix

--- utils/socket.js
+++ utils/socket.js
@@ -39,7 +39,7 @@ function joinResponse(roomInfo, io, socket){
         progress: "waiting"
     };
     socket.emit("name", playerName);
-    io.to(newRoomName).emit("join", participantList);
+    io.to(newRoomName).emit("changeMember", participantList);
 }
 
 function exitResponse(roomInfo, io, socket){
