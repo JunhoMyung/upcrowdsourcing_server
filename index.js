e755c6a96306e5235bc5a9f476c25c211cdcc9e6
JunhoMyung <74249745+JunhoMyung@users.noreply.github.com>
2021-08-26 05:19:58 +0900

    confirmation

--- index.js
+++ index.js
@@ -59,12 +59,40 @@ io.on("connection", (socket) => {
   socket.on("submitDescription", (temp) => {
     io.to(socket.roomName).emit("addescription", temp)
   })
+  socket.on("confirmation", (playerName) => {
+    io.to(socket.roomName).emit("confirmation", playerName)
+    roomInfo[socket.roomName]["agree"] = 0
+    roomInfo[socket.roomName]["disagree"] = 0
+  })
   socket.on("finish", () => {
     io.to(socket.roomName).emit("finish")
     roomInfo[socket.roomName]["progress"] = "survey";
   })
   socket.on("reaction", (emoji, name, num) => {
     io.to(socket.roomName).emit("reaction", emoji, name, num)
+  })
+  socket.on("agree", (player) => {
+    io.to(socket.roomName).emit("agree", player)
+    const temp = roomInfo[socket.roomName]["agree"]
+    roomInfo[socket.roomName]["agree"] = temp + 1
+    if (temp + 1 == 3) {
+      io.to(socket.roomName).emit("finish")
+      roomInfo[socket.roomName]["progress"] = "survey";
+    }
+    else if((temp + 1 + roomInfo[socket.roomName]["disagree"]) == 4){
+      io.to(socket.roomName).emit("nonagreement")
+    }
+  })
+  socket.on("disagree", (player) => {
+    io.to(socket.roomName).emit("disagree", player)
+    const temp = roomInfo[socket.roomName]["disagree"]
+    roomInfo[socket.roomName]["disagree"] = temp + 1
+    if (temp + 1 == 2) {
+      io.to(socket.roomName).emit("nonagreement")
+    }
+    else if((temp + 1 + roomInfo[socket.roomName]["agree"]) == 4){
+      io.to(socket.roomName).emit("nonagreement")
+    }
   })
   socket.on("refuse", () => {
     if (socket.roomName != null){
@@ -81,6 +109,9 @@ io.on("connection", (socket) => {
       io.to(temp).emit("newMember", roomInfo[temp]["ready"]);
     }
     socket.roomName = null;
+  })
+  socket.on("acceptfail", () => {
+    roomInfo[socket.roomName]["accept"] = 0;
   })
   socket.on("end-waiting", () => {
     if (socket.roomName != null){
